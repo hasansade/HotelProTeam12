a5e38d16abdd089ede32f596d1c08b3f2e5fa681
hasan <hsnkale2021@gmail.com>
2022-02-10 21:27:38 +0300

    v1

---
+++ src/test/java/utilities/ReusableMethods.java
@@ -0,0 +1,79 @@
+package utilities;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import pages.MainPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ReusableMethods {
+
+    public ReusableMethods() {
+    }
+
+    public static void goToUrl() {
+        Driver.getDriver().get(ConfigReader.getProperty("mainUrl"));
+    }
+
+
+/*
+    public static void login() {
+        MainPage mainPage = new MainPage();
+        LoginPage loginPage = new LoginPage();
+        goToUrl();
+        mainPage.loginLink.click();
+        if (!Driver.getDriver().getCurrentUrl().equals(ConfigReader.getProperty("loginUrl"))) {
+            goToUrl();
+            mainPage.loginLink.click();
+        }
+
+        loginPage.usernameBox.sendKeys(new CharSequence[]{ConfigReader.getProperty("validUserName")});
+        loginPage.passwordBox.sendKeys(new CharSequence[]{ConfigReader.getProperty("validPassword")});
+        loginPage.loginButton.click();
+    }
+
+ */
+
+/*
+    public static void hotelRooms() {
+        DefaultPage defaultPage = new DefaultPage();
+        defaultPage.hotelManagementLinki.click();
+        defaultPage.hotelRoomsLinki.click();
+    }
+
+ */
+    public static void waitFor(int sec) {
+        try {
+            Thread.sleep((long)(sec * 1000));
+        } catch (InterruptedException var2) {
+            var2.printStackTrace();
+        }
+
+    }
+
+    public static void scrollTo(WebElement element) {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor)Driver.getDriver();
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", new Object[]{element});
+        waitFor(2);
+    }
+
+    public static String getScreenshot(String name) throws IOException {
+        String date = (new SimpleDateFormat("dd.MM.yyyy.hh.mm.ss")).format(new Date());
+        TakesScreenshot takesScreenshot = (TakesScreenshot)Driver.getDriver();
+        File source = (File)takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+        File finalDestination = new File(target);
+        FileUtils.copyFile(source, finalDestination);
+        return target;
+    }
+
+
+    }
+
+
