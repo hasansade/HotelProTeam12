45a188a4173f801276104212dd4190c805dd701e
usernameSinan <talhau@mail.com>
2022-02-12 23:05:39 +0300

    talhaa

--- src/test/java/tests/US_004/TC_001.java
+++ src/test/java/tests/US_004/TC_001.java
@@ -1,6 +1,13 @@
 package tests.US_004;
 
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
+import utilities.TestBase;
 
-public class TC_001 {
+public class TC_001 extends TestBase{
+    @Test
+    public void test() {
 
+        ReusableMethods.login();
+    }
 }
