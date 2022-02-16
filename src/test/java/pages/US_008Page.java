1fbabbc9cc3beea19340b07c4b0598924192b2bc
hasan <hsnkale2021@gmail.com>
2022-02-17 00:39:04 +0300

    v1

--- src/test/java/pages/US_008Page.java
+++ src/test/java/pages/US_008Page.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -9,4 +11,39 @@ public class US_008Page {
         PageFactory.initElements(Driver.getDriver(), this);
 
     }
+
+    @FindBy (xpath = "//span[text()='System Management']")
+    public WebElement systemManagementLinki;
+
+    @FindBy(xpath = "//span[text()='Hotel Management']")
+    public WebElement hotelManagementLinki;
+
+    @FindBy(partialLinkText = "Room reservation")
+    public WebElement roomReservationLinki;
+
+    @FindBy(xpath = "//span[text()='Add Room Reservation ']")
+    public WebElement addRoomReservationButonu;
+
+@FindBy(xpath = "//span[@class='caption-subject font-green-sharp bold uppercase']")
+    public WebElement listOfReservationsLinki;
+
+    @FindBy(id = "IDUser")
+    public WebElement selectUserDropDown;
+
+    @FindBy(id = "IDHotelRoom")
+    public WebElement selectHotelDropDown;
+
+    @FindBy(id= "Price")
+    public WebElement priceBox;
+
+    @FindBy(id= "btnSubmit")
+    public WebElement createHotelroomreservationSaveButonu;
+
+    @FindBy(xpath = "//div[@class='bootbox-body']")
+    public WebElement hotelWasInsertedSuccessfullyYazısı;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement hotelWasInsertedSuccessfullyOkButonu;
+
+
 }
