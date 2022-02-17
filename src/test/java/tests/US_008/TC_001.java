823bf057769cd67705a60bd9f2f19f2e072bb2bb
hasan <hsnkale2021@gmail.com>
2022-02-17 11:34:33 +0300

    clonePass

--- src/test/java/tests/US_008/TC_001.java
+++ src/test/java/tests/US_008/TC_001.java
@@ -1,7 +1,17 @@
 package tests.US_008;
 
-public class TC_001 {
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
+public class TC_001 extends TestBaseRapor {
 
+    public TC_001() {
+    }
 
+    @Test
+    public void test() {
+        ReusableMethods.login();
+        extentTest= extentReports.createTest("TC_001 anasayfaya Login olundu");
+    }
 }
